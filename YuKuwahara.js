cd95b30547b137749d89d203748dd6ac21b219d5
kuwahara <kuwahara@MacBook-no-MacBook-Pro.local>
2020-08-10 17:14:01 +0900

    Portfolio中間コミット(未完成)〜ブログ、案件、JSなど追加予定〜

--- YuKuwahara.js
+++ YuKuwahara.js
@@ -1,11 +1,14 @@
-var navPos = jQuery("#global-nav").offset().top; // グローバルメニューの位置
+// if (jQuery("#global-nav").length) {
+//   var navPos = jQuery("#global-nav").offset({ top: 700});
+// }
+var navPos = jQuery("#global-nav").offset({ top: 700, left: 0 }); // グローバルメニューの位置
 var navHeight = jQuery("#global-nav").outerHeight(); // グローバルメニューの高さ
 jQuery(window).on("scroll", function () {
   if (jQuery(this).scrollTop() > navPos) {
     jQuery("body").css("padding-top", navHeight);
     jQuery("#global-nav").addClass("m_fixed");
   } else {
-    jQuery("body").css("padding-top", 0);
+    jQuery("body").css("pa    dding-top", 0);
     jQuery("#global-nav").removeClass("m_fixed");
   }
 });
